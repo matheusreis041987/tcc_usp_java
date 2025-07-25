f0905dd6d43172ff0940c2068364515c71a879ca
matheusreis041987 <matheusgithub1987@gmail.com>
2025-07-25 19:58:50 -0300

    Primeiro commit do projeto tcc usp

---
+++ usp/src/main/java/com/tcc/usp/UspApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.usp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspApplication.class, args);
+	}
+
+}
